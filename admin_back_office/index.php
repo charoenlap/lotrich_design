b4275f5ef3d9c52dd18c651416d2dd1a47e80226
root <root@lotrich.lotrich168.com>
2022-03-04 07:17:35 +0000

    update

--- admin_back_office/index.php
+++ admin_back_office/index.php
@@ -15,9 +15,9 @@
 		require_once('catalog/setup.php'); 
 		require_once($_SERVER['DOCUMENT_ROOT'].'/lotrich/lib/system/loader/autoload.php'); 
 	}else{
-		require_once('/home/admin/domains/lotrich168.com/config/domains/lotrich/config.php'); 
-		require_once('/home/admin/domains/lotrich168.com/lib/function/main_function.php');
-		require_once('/home/admin/domains/lotrich168.com/public_html/admin_back_office/catalog/setup.php'); 
-		require_once('/home/admin/domains/lotrich168.com/lib/system/loader/autoload.php'); 
+		require_once('/home/admin/domains/numchai365.com/config/domains/lotrich/config.php'); 
+		require_once('/home/admin/domains/numchai365.com/lib/function/main_function.php');
+		require_once('/home/admin/domains/numchai365.com/public_html/admin_back_office/catalog/setup.php'); 
+		require_once('/home/admin/domains/numchai365.com/lib/system/loader/autoload.php'); 
 	}
 ?>
